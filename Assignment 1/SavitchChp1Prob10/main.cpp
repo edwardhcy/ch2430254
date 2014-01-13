a755856408e9f8c0bf6dc0a8855bcf2a2c851bb6
edwardhcy <edwardhcy@gmail.com>
2014-01-13 04:03:47 -0800

    Signed-off-by: edwardhcy <edwardhcy@gmail.com>

---
+++ Assignment 1/SavitchChp1Prob10/main.cpp
@@ -0,0 +1,37 @@
+/* 
+ * File:   main.cpp
+ * Author: Chanyap Ho
+ * Created on January 13, 2014, 3:30 AM
+ * Savitch Chapter 1 Problem 10
+ */
+
+#include <iostream>
+using namespace std;
+
+//Global Constant
+
+//Function Prototype
+
+//Execution begins here
+
+int main() {
+    //declare variable
+    char crtC;
+    cout<<"Please enter the character you like to use in ";
+    cout<<"creating a large C"<<endl;
+    cin>>crtC;
+    
+    //Output
+    cout<<"  "<<crtC<<crtC<<crtC<<endl;
+    cout<<" "<<crtC<<"  "<<crtC<<endl;
+    cout<<crtC<<endl;
+    cout<<crtC<<endl;
+    cout<<crtC<<endl;
+    cout<<crtC<<endl;
+    cout<<crtC<<endl;
+    cout<<" "<<crtC<<"  "<<crtC<<endl;
+    cout<<"  "<<crtC<<crtC<<crtC<<endl;
+    //Exit stage right
+    return 0;
+}
+
---
+++ Assignment 1/SavitchChp1Prob7/main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   main.cpp
+ * Author: Chanyap Ho
+ * Created on January 13, 2014, 3:37 AM
+ * Savitch Chapter 1 Problem 7
+ */
+
+#include <iostream>
+using namespace std;
+
+//Global Constant
+
+//Function Prototype
+
+//Execution begins here
+int main() {
+    cout<<"**************************************"<<endl;
+    cout<<endl;
+    //typed this completely by hand, isn't there a better way of doing it?
+    cout<<"      "<<"CCC"<<"         "<<"SSSS"<<"       "<<"!!\n";        
+    cout<<"    "<<"C     C"<<"      "<<"S     S"<<"     "<<"!!\n";
+    cout<<"   "<<"C    "<<"        "<<"S"<<"            "<<"!!\n";
+    cout<<"  "<<"C    "<<"          "<<"S"<<"           "<<"!!\n";
+    cout<<"  "<<"C    "<<"           "<<"SSSS"<<"       "<<"!!\n";
+    cout<<"  "<<"C    "<<"                "<<"S"<<"     "<<"!!\n";
+    cout<<"   "<<"C    "<<"          "<<"    S"<<"      "<<"!!\n";
+    cout<<"    "<<"C     C"<<"    "<<"S      S"<<"      "<<"  \n";
+    cout<<"      "<<"CCC"<<"        "<<"SSSS"<<"        "<<"00\n";
+    cout<<endl;
+    cout<<"**************************************"<<endl;
+    cout<<endl;
+    cout<<"Computer Science is Cool Stuff!!!"<<endl;
+    
+    return 0;
+}
+
