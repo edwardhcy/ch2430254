09a00be96c1d67b4cd844f49fa32cc4462af705b
edwardhcy <edwardhcy@gmail.com>
2014-01-12 23:09:45 -0800

    Signed-off-by: edwardhcy <edwardhcy@gmail.com>

---
+++ Assignment 1/SavitchChp1Prob5/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Chanyap Ho 
+ * Created on January 12, 2014, 10:55 PM
+ * Savitch Chapter 1 Question 5
+ */
+
+#include <iostream>
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main() {
+    //declare variable
+    int ITGR1,ITGR2,SUM,PDT;
+    //prompt for 1st integer
+    cout <<"Please enter your 1st integer.\n";
+    cin>>ITGR1;
+    //prompt for 2nd integer
+    cout <<"Please enter your 2nd integer.\n";
+    cin>>ITGR2;
+    //formula
+    SUM=ITGR1+ITGR2;
+    PDT=ITGR1*ITGR2;
+    //result
+    cout <<"The sum of your 1st and 2nd integer is "<<SUM<<endl;
+    cout <<"The product of your 1st and 2nd integer is "<<PDT<<endl;
+    
+    return 0;
+}
+
